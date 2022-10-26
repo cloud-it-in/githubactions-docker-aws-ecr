d7f412aa0ecf88ea55da481192df2b497018c881
PRADIP DALVI <61991235+cloud-it-in@users.noreply.github.com>
2022-10-26 16:49:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/status', (req, res)=>{
 
 app.get('/hello',(req, res)=>{
     res.status(200)
-    res.send('Hi, there!')
+    res.send('Hi, there. This is a Docker nodejs test!')
 
 })
 
